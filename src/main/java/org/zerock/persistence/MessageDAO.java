62ff1c1404bcd51c4a4db563ce985ae3fa305c1c
SeyoonMsi <cd1r@na>
2017-03-13 17:31:47 +0900

    msi commit

---
+++ src/main/java/org/zerock/persistence/MessageDAO.java
@@ -0,0 +1,12 @@
+package org.zerock.persistence;
+
+import org.zerock.domain.MessageVO;
+
+public interface MessageDAO {
+
+	public void create(MessageVO vo) throws Exception;
+	public MessageVO readMessage(int mid) throws Exception;
+	public void updateState(int mid) throws Exception;
+	
+	
+}
